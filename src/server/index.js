a1213712f668f203afdbb48c1c8d9ef85669c359
win <khacyle@gmail.com>
2022-08-26 17:08:14 +0700

    feat: change package.json

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,7 @@ import  App from '../client/pages/index.js';
 import template from './template';
 
 import express from 'express';
-import { matchPath, StaticRouter } from 'react-router-dom';
 import * as ReactDOMServer from 'react-dom/server';
-import routes from '../client/routes.js';
-import * as path from 'path';
 
 const app = express();
 
